2138cbb7a5da238e2997b4bfd6d377bbde0ddffc
Cupapimumenyo <yusuf.setiobudi2006@gmail.com>
2026-02-14 13:45:38 +0700

    fix: pivot gender filtering from facility level to facility_item level
    - Update showSchedule to handle generic facility names like 'Mesin Cuci'.
    - Apply gender constraints on the facilityItem relationship instead of facility name.
    - Improve query accuracy for shared facilities with gendered equipment.

--- resources/views/penghuni/viewSchedule.blade.php
+++ resources/views/penghuni/viewSchedule.blade.php
@@ -152,6 +152,7 @@
                                 @endforeach
                             </tbody>
                         </table>
+                        {{  }}
                     </div>
                 </div>
             </div>
